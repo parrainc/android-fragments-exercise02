b49e884528c2ce399750baa4c8e0e9ee05405782
Carlos Parra <carlos.parra.sbr@gmail.cm>
2018-06-12 00:32:57 -0400

    Implemented fragments communication thru main activity

--- app/src/main/java/com/example/carlosparra/fragmentsexercise2/activities/MainActivity.java
+++ app/src/main/java/com/example/carlosparra/fragmentsexercise2/activities/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.carlosparra.fragmentsexercise2.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
 import com.example.carlosparra.fragmentsexercise2.R;
+import com.example.carlosparra.fragmentsexercise2.fragments.LeftFragment;
+import com.example.carlosparra.fragmentsexercise2.fragments.RightFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements LeftFragment.LeftFragmentListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void sendData(String data) {
+        RightFragment rightFragment = (RightFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragmentRight);
+        rightFragment.setText(data);
     }
 }
